a76aea3d090caf7e4e97ed7a5dd5bb7874b04851
Ryan <tamielryan@gmail.com>
2022-01-19 15:35:11 +0200

    Initial Commit

---
+++ TheMillions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMillions
+//
+//  Created by Ryan J. W. Kim on 2022/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMillions/TheMillionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMillionsApp.swift
+//  TheMillions
+//
+//  Created by Ryan J. W. Kim on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct TheMillionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
