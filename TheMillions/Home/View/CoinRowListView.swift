6ab7dbcdb5c9e8af4c048697bbc3b8f77e56a6e2
Ryan <tamielryan@gmail.com>
2022-01-19 20:07:35 +0200

    CoinRowListView

--- TheMillions/Home/View/CoinRowListView.swift
+++ TheMillions/Home/View/CoinRowListView.swift
@@ -10,9 +10,27 @@ import SwiftUI
 struct CoinRowListView: View {
     // MARK: - Properties
     let coin: Coin
+    let showHoldingsColumn: Bool
     
     // MARK: - Body
     var body: some View {
+        GeometryReader { geo in
+            HStack(spacing: 0) {
+                leftColumn
+                Spacer()
+                if showHoldingsColumn {
+                    centerColumn
+                }
+                rightColumn
+                    .frame(width: geo.size.width / 3)
+                //                .frame(width: UIScreen.main.bounds.width / 3)
+            } //: HStack
+            .font(.subheadline)
+        } //: Geo
+    }
+}
+extension CoinRowListView {
+    private var leftColumn: some View {
         HStack(spacing: 0) {
             Text("\(coin.rank)")
                 .font(.caption)
@@ -24,22 +42,33 @@ struct CoinRowListView: View {
                 .font(.headline)
                 .padding(.leading, 5)
                 .foregroundColor(.theme.accent)
-            Spacer()
-            VStack {
-                Text("\(coin.currentPrice)")
-                    .bold()
-                Text("\(coin.priceChangePercentage24H ?? 0)%")
-                    .foregroundColor(
-                        (coin.priceChangePercentage24H ?? 0) >= 0 ?
-                            .theme.green : .theme.red
-                    )
-            }
         }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingValue.currencyTo2Digits())
+                .bold()
+            Text((coin.currentHoldings ?? 0).numberTo6Digits())
+        }
+        .foregroundColor(.theme.accent)
+    }
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.currencyTo2Digits())
+                .bold()
+            Text(coin.priceChangePercentage24H?.percentToString() ?? "0%")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                        .theme.green : .theme.red
+                )
+        } //: VStack
     }
 }
 
 struct CoinRowListView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowListView(coin: Coin.example)
+        CoinRowListView(coin: Coin.example, showHoldingsColumn: true)
+        
     }
 }
