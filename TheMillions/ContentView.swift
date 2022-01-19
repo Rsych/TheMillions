833fc1932e394d87423cf9c8a50d3e92a8cf5e61
Ryan <tamielryan@gmail.com>
2022-01-19 15:50:33 +0200

    ColorExtension

--- TheMillions/ContentView.swift
+++ TheMillions/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack(spacing: 40) {
+                Text("Accent Color")
+                    .foregroundColor(.theme.accent)
+                Text("Secondary Color")
+                    .foregroundColor(.theme.secondaryText)
+                Text("red Color")
+                    .foregroundColor(.theme.red)
+                Text("green Color")
+                    .foregroundColor(.theme.green)
+            }
+            .font(.headline)
+        } //: ZStack
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
---
+++ TheMillions/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  TheMillions
+//
+//  Created by Ryan J. W. Kim on 2022/01/19.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
