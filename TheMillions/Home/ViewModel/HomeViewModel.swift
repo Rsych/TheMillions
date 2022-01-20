d7e71c39013a5bee0a363542c6eaa0d8bcf389cc
Ryan <tamielryan@gmail.com>
2022-01-20 14:02:15 +0200

    Search combine

--- TheMillions/Home/ViewModel/HomeViewModel.swift
+++ TheMillions/Home/ViewModel/HomeViewModel.swift
@@ -22,10 +22,29 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] (returnedCoins) in
-                self?.allCoins = returnedCoins
+        // update allCoins
+        $searchText
+        // subscribed to both searchText and allcoins, so any change, this is getting published
+            .combineLatest(dataService.$allCoins)
+        // wait 0.5 to prevent too fast request from user
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] (returnedCoin) in
+                self?.allCoins = returnedCoin
             }
             .store(in: &cancellable)
+    }
+    
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
