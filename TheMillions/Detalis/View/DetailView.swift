43c945edcf355e48b1d7b1cbbe316a9a0c782cb0
Ryan <tamielryan@gmail.com>
2022-01-23 13:26:41 +0200

    Detail VM done

--- TheMillions/Detalis/View/DetailView.swift
+++ TheMillions/Detalis/View/DetailView.swift
@@ -26,7 +26,11 @@ struct DetailLoadingView: View {
 
 struct DetailView: View {
     // MARK: - Properties
-    @StateObject var vm: DetailViewModel
+    @StateObject private var vm: DetailViewModel
+    private let column: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     init(coin: Coin) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
@@ -34,14 +38,63 @@ struct DetailView: View {
     }
     // MARK: - Body
     var body: some View {
-        ZStack {
-           Text("Hello")
-        } //: ZStack
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Hi")
+                    .frame(height: 150)
+                
+                overViewTitle
+                Divider()
+                overViewGrid
+                additionalTitles
+                Divider()
+                additionalViewGrid
+            } //: VStack
+            .padding()
+        } //: ScrollView
+        .navigationTitle(vm.coin.name)
     }
 }
 
+
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: Coin.example)
+        NavigationView {
+            DetailView(coin: Coin.example)
+        }
+    }
+}
+
+extension DetailView {
+    private var overViewTitle: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var additionalTitles: some View {
+        Text("Additional Detail")
+            .font(.title)
+            .bold()
+            .foregroundColor(.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var overViewGrid: some View {
+        LazyVGrid(columns: column, alignment: .leading, spacing: 30, pinnedViews: []) {
+            ForEach(vm.overviewStats) { stat in
+                StatsView(stats: stat)
+            }
+        } //: LazyVGrid
+    }
+    
+    private var additionalViewGrid: some View {
+        LazyVGrid(columns: column, alignment: .leading, spacing: 30, pinnedViews: []) {
+            ForEach(vm.additionalStats) { stat in
+                StatsView(stats: stat)
+            }
+        } //: LazyVGrid
     }
 }
--- TheMillions/Detalis/ViewModel/DetailViewModel.swift
+++ TheMillions/Detalis/ViewModel/DetailViewModel.swift
@@ -9,20 +9,79 @@ import Foundation
 import Combine
 
 class DetailViewModel: ObservableObject {
+    
+    @Published var overviewStats: [Stats] = []
+    @Published var additionalStats: [Stats] = []
+    
+    @Published var coin: Coin
     private let coinDetailService: CoinDetailService
     private var cancellables = Set<AnyCancellable>()
     
     init(coin: Coin) {
+        self.coin = coin
         self.coinDetailService = CoinDetailService(coin: coin)
         self.addSubscribers()
+        
     }
     
     private func addSubscribers() {
         coinDetailService.$coinDetails
-            .sink { returnedCoinDetails in
+            .combineLatest($coin)
+            .map({ (coinDetailModel, coinModel) -> (overview: [Stats], additional: [Stats]) in
+                // Overview
+                let price = coinModel.currentPrice.currencyTo2Digits()
+                let pricePercentChange = coinModel.priceChangePercentage24H
+                let pricePercentStat = Stats(title: "Price Changes in %", value: price, percentile: pricePercentChange)
+                
+                let marketCap = "$" + (coinModel.marketCap?.formattedWithAbbreviations() ?? "")
+                let marketCapPercentChange = coinModel.marketCapChangePercentage24H
+                let marketCapPercentStat = Stats(title: "Market Capitalisation", value: marketCap, percentile: marketCapPercentChange)
+                
+                let rank = "\(coinModel.rank)"
+                let rankStat = Stats(title: "Rank", value: rank)
+                
+                let volume = "$" + (coinModel.totalVolume?.formattedWithAbbreviations() ?? "")
+                let volumeStat = Stats(title: "Volume", value: volume)
+                
+                let overviewArray: [Stats] = [
+                    pricePercentStat, marketCapPercentStat, rankStat, volumeStat
+                ]
+                
+                // Additional
+                let high = coinModel.high24H?.currencyTo2Digits() ?? "N/A"
+                let highStats = Stats(title: "24 High", value: high)
+                
+                let low = coinModel.low24H?.currencyTo2Digits() ?? "N/A"
+                let lowStats = Stats(title: "24 Low", value: low)
+                
+                let priceChange = coinModel.priceChange24H?.currencyTo2Digits() ?? "N/A"
+                let pricePercentChange2 = coinModel.priceChangePercentage24H
+                let priceChangeStat = Stats(title: "24h Price Change", value: priceChange, percentile: pricePercentChange2)
+                
+                let marketCapChange = "$" + (coinModel.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+                let marketCapPercentChange2 = coinModel.marketCapChangePercentage24H
+                let marketCapChangeStat = Stats(title: "24h Market Cap Change", value: marketCapChange, percentile: marketCapPercentChange2)
+                
+                let blockTime = coinDetailModel?.blockTimeInMinutes ?? 0
+                let blockTimeString = blockTime == 0 ? "N/A" : String(blockTime)
+                let blockStat = Stats(title: "Block Time", value: blockTimeString)
+                
+                let hashing = coinDetailModel?.hashingAlgorithm ?? "N/A"
+                let hashingStat = Stats(title: "Hashing Algorithm", value: hashing)
+                
+                let additionalArray: [Stats] = [
+                    highStats, lowStats, priceChangeStat, marketCapChangeStat, blockStat, hashingStat
+                ]
+                
+                return (overviewArray, additionalArray)
+            })
+            .sink { [weak self] returnedArrays in
                 print("Received coin detail data")
-                print(returnedCoinDetails)
+                self?.overviewStats = returnedArrays.overview
+                self?.additionalStats = returnedArrays.additional
             }
             .store(in: &cancellables)
     }
+    
+    
 }
