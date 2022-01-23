4430b649d99d17bd1196dc567dc7e4649a0c3649
Ryan <tamielryan@gmail.com>
2022-01-23 23:11:22 +0200

    SettingsViewInit

---
+++ TheMillions/Settings/View/SettingsView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsView.swift
+//  TheMillions
+//
+//  Created by Ryan J. W. Kim on 2022/01/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .navigationTitle("Settings")
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
